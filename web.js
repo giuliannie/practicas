eaae78359821300d9b66e2af3b6b0926dc2096fe
Giuliani Perry <perryg.88@gmail.com>
2011-08-23 15:30:45 -0600

    prueba calculadora

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var operations = {
+    add: function(a,b){return a+b},
+    sub: function(a,b){return a-b},
+    mul: function(a,b){return a*b},
+    div: function(a,b){return a/b}
+}
+
+app.get('/:op/:a/:b', function(request, response) {
+  op = operations[request.params.op],
+  var result = op ? op(parseInt(request.params.a,10),
+  parseInt(request.params.b,10)) :  "Error";
+  
+  response.send(''+result);
 });
 
 var port = process.env.PORT || 3000;
